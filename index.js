9097036c7850130e524d9804325ecee0645c797b
teppeis <teppeis@gmail.com>
2016-02-03 02:58:23 +0900

    implement

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 'use strict';
 
+function resolve(fn) {
+  return new Promise(res => {
+    res(fn());
+  });
+}
+
 function whilst(condition, action) {
+  return resolve(function loop(res) {
+    return resolve(() => {
+      if (condition()) {
+        return resolve(() => {
+          return action();
+        }).then(loop);
+      } else {
+        return res;
+      }
+    });
+  });
 };
 
 module.exports = whilst;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,55 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('power-assert');
 const whilst = require('../');
 
 describe('whilst', () => {
-  it('is a function', () => {
+  it('should be a function', () => {
     assert(typeof whilst === 'function');
+  });
+
+  it('with a sync action', () => {
+    let result = [];
+    let i = 0;
+    return whilst(() => i < 3, () => {
+      result.push(i);
+      return i++;
+    }).then(finalResult => {
+      assert.deepEqual(result, [0, 1, 2]);
+      assert.equal(finalResult, 2);
+    });
+  });
+
+  it('with an async action', () => {
+    let result = [];
+    let i = 0;
+    return whilst(() => i < 3, () => {
+      return Promise.resolve().then(() => {
+        result.push(i);
+        return i++;
+      });
+    }).then(finalResult => {
+      assert.deepEqual(result, [0, 1, 2]);
+      assert.equal(finalResult, 2);
+    });
+  });
+
+  it('with an async error', (done) => {
+    let result = [];
+    let i = 0;
+    return whilst(() => i < 3, () => {
+      return Promise.resolve().then(() => {
+        if (i > 1) {
+          throw new Error('foo');
+        }
+        result.push(i);
+        return i++;
+      });
+    }).catch(e => {
+      assert.deepEqual(result, [0, 1]);
+      assert(e instanceof Error);
+      assert.equal(e.message, 'foo');
+      done();
+    });
   });
 });
